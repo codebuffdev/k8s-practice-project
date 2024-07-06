14b4701cbd3822027267e1e9b06010d7c208c9af
codebuffdev <codebuffdev@gmail.com>
2024-07-06 13:34:43 +0530

    project to check livelinessProbe & readinessProbe on pods through spring boot actuator endpoints

---
+++ readiness-liveness-probe/src/main/java/in/codebuffdev/readinesslivenessprobe/ReadinessLivenessProbeApplication.java
@@ -0,0 +1,13 @@
+package in.codebuffdev.readinesslivenessprobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadinessLivenessProbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadinessLivenessProbeApplication.class, args);
+    }
+
+}
---
+++ readiness-liveness-probe/src/test/java/in/codebuffdev/readinesslivenessprobe/ReadinessLivenessProbeApplicationTests.java
@@ -0,0 +1,13 @@
+package in.codebuffdev.readinesslivenessprobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadinessLivenessProbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
